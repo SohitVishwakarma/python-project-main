1468989eab5abab4551c284b5af4852d5b49481e
akash <akash@arivani.net>
2024-05-01 12:48:07 +0530

    Another with Test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,3 @@
-from app import app
-
-
 def test_index():
     # Create a test client using Flask's test_client method
     client = app.test_client()
@@ -8,5 +5,5 @@ def test_index():
     # Use the client to simulate accessing the '/' route
     response = client.get('/')
 
-    # Check if the response data matches the expected string
-    assert response.data == b"Hello, worlfdfddixu8uxquixhqisxhqsoxh isxhs xquisxh qsihx hqs xix ishwxh ddddddddddd!"
+    # Check if the expected text is present in the response data
+    assert b"Hello, worlfdfddixu8uxquixhqisxhqsoxh isxhs xquisxh qsihx hqs xix ishwxh ddddddddddd!" in response.data
