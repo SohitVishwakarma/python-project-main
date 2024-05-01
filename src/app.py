e32b32e2da3668de6743278dacb01e3d1d5dcdb0
akash <akash@arivani.net>
2024-05-01 12:50:08 +0530

    Past

--- src/app.py
+++ src/app.py
@@ -4,56 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return """
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Hello World</title>
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                background-color: #f0f0f0;
-            }
+    return "Hello, worlfdfddixu8uxquixhqisxhqsoxh isxhs xquisxh qsihx hqs xix ishwxh ddddddddddd!"
 
-            .container {
-                width: 80%;
-                margin: auto;
-                text-align: center;
-                padding-top: 50px;
-            }
-
-            h1 {
-                color: #333;
-            }
-
-            .btn {
-                background-color: #007bff;
-                color: #fff;
-                border: none;
-                padding: 10px 20px;
-                cursor: pointer;
-                border-radius: 5px;
-                font-size: 16px;
-            }
-
-            .btn:hover {
-                background-color: #0056b3;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <h1>Hello, World!</h1>
-            <p>Welcome to my Flask application.</p>
-            <div class="button-container">
-                <button class="btn">Click Me</button>
-            </div>
-        </div>
-    </body>
-    </html>
-    """
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
+
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,6 @@
+from app import index
+
+
 def test_index():
-    # Create a test client using Flask's test_client method
-    client = app.test_client()
+    assert index() == "Hello, worlfdfddixu8uxquixhqisxhqsoxh isxhs xquisxh qsihx hqs xix ishwxh ddddddddddd!"
 
-    # Use the client to simulate accessing the '/' route
-    response = client.get('/')
-
-    # Check if the expected text is present in the response data
-    assert b"Hello, worlfdfddixu8uxquixhqisxhqsoxh isxhs xquisxh qsihx hqs xix ishwxh ddddddddddd!" in response.data
